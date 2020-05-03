5b57078dbbe530cc2529553ce3554a1f5032e5d9
Nicholas Davies <nicholasjohndavies42@gmail.com>
2020-05-04 02:12:17 +0800

    added api for notes

--- Develop/public/assets/js/server.js
+++ Develop/public/assets/js/server.js
@@ -1,6 +1,7 @@
 // Dependencies
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 // local dependencies
 const notes = require("../../../db/db.json")
@@ -17,20 +18,27 @@ app.use(express.json());
 // ======================================
 // // // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../index.js"));
-  });
+    res.sendFile(path.join(__dirname, "../../index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../../notes.html"));
+});
 // ======================================
 
 // API ROUTES
 // ======================================
 app.get("/api/notes", function(req, res) {
-    return res.notes
-  });
+    const jsonpath = path.join(__dirname, "../../../db/db.json");
+    console.log(jsonpath);
+    const data = fs.readFileSync(jsonpath, "utf8");
+    return res.send(data);
+});
 // ======================================
 
 // Starts the server to begin listening
 // ======================================
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
-  });
+});
 // ======================================
